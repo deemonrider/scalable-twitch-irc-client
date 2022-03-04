8ca2988295e5448c5badfa15cc1459dbebde61d4
Malte <DeemonRider@gmx.de>
2022-03-04 16:57:11 +0100

    add requirements

--- setup.py
+++ setup.py
@@ -3,10 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name='TwitchClient',
     version='0.0.1',
-    url='https://github.com/deemonrider/twitch-chat-manager.git',
+    url='https://github.com/deemonrider/scalable-twitch-irc-client.git',
     author='DeemonRider',
     author_email='',
     description='-',
     packages=find_packages(),
-    install_requires=[],
+    install_requires=[
+        "requests==2.27.1"
+    ],
 )
