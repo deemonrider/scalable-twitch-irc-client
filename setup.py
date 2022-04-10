2ed9e47c36a577cf971049372405e1e38be59ea9
Malte <DeemonRider@gmx.de>
2022-04-11 00:24:27 +0200

    track channel_name

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ScalableTwitchIrcClient',
-    version='0.1.7',
+    version='0.1.8',
     url='https://github.com/deemonrider/scalable-twitch-irc-client.git',
     author='DeemonRider',
     author_email='',
