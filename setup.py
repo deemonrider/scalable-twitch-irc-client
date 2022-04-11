b276fa093c539e5c7d8b652a92f429b2247b2043
Malte <DeemonRider@gmx.de>
2022-04-12 00:47:44 +0200

    dont log whisper

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ScalableTwitchIrcClient',
-    version='0.1.9',
+    version='0.2.0',
     url='https://github.com/deemonrider/scalable-twitch-irc-client.git',
     author='DeemonRider',
     author_email='',
