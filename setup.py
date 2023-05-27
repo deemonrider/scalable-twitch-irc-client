2bbecb2500bdaae9eec35199e82c1329ae6b00a3
Malte <DeemonRider@gmx.de>
2023-05-27 13:47:15 +0200

    bump requirements

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='scalable-twitch-irc-client',
-    version='0.4.0',
+    version='0.4.1',
     url='https://github.com/deemonrider/scalable-twitch-irc-client.git',
     author='DeemonRider',
     author_email='',
     description='-',
     packages=find_packages(),
     install_requires=[
-        "requests==2.28.2"
+        "requests==2.31.0"
     ],
 )
