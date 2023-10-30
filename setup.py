7927b39104726fb50b329b5b053345586db366d9
Malte <DeemonRider@gmx.de>
2023-10-30 22:51:17 +0100

    rm websocket requirement

--- setup.py
+++ setup.py
@@ -2,14 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='scalable-twitch-irc-client',
-    version='0.7.1',
+    version='0.7.2',
     url='https://github.com/deemonrider/scalable-twitch-irc-client.git',
     author='DeemonRider',
     author_email='',
     description='-',
     packages=find_packages(),
     install_requires=[
-        "requests==2.31.0",
-        "websockets==12.0",
+        "requests==2.31.0"
     ],
 )
